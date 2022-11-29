97e9e1512beaca8d49a53484ec98e0e652085062
DrabantBY <drabant2022@gmail.com>
2022-11-29 12:51:33 +0300

    feat: implement hash function

--- nodejs-basics/src/hash/calcHash.js
+++ nodejs-basics/src/hash/calcHash.js
@@ -1,5 +1,12 @@
+import { getFilePath } from '../getFilePath.js';
+import { createReadStream } from 'fs';
+import { createHash } from 'crypto';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const targetFile = getFilePath(import.meta.url, 'files/fileToCalculateHashFor.txt');
+  const readStream = createReadStream(targetFile);
+  const hash = createHash('sha256');
+  readStream.pipe(hash).setEncoding('hex').pipe(process.stdout);
 };
 
 await calculateHash();
