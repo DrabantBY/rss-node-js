d4f4b12be448a8fd2d7c3ce7a2b99edcfd59f969
DrabantBY <drabant2022@gmail.com>
2022-11-30 01:31:53 +0300

    refactor: update copy function

--- nodejs-basics/src/fs/copy.js
+++ nodejs-basics/src/fs/copy.js
@@ -1,13 +1,23 @@
 import { getFilePath } from '../getFilePath.js';
-import { cp } from 'fs/promises';
+import { readdir, mkdir, copyFile } from 'fs/promises';
+import { join } from 'path';
+// import { cp } from 'fs/promises';
 
 const copy = async () => {
-  const sourceFolder = getFilePath(import.meta.url, 'files');
   const targetFolder = getFilePath(import.meta.url, 'files_copy');
-  const options = { recursive: true, force: false, errorOnExist: true };
-
+  const sourceFolder = getFilePath(import.meta.url, 'files');
+  // const options = { recursive: true, force: false, errorOnExist: true };
   try {
-    await cp(sourceFolder, targetFolder, options);
+    // await cp(sourceFolder, targetFolder, options);
+    await mkdir(targetFolder);
+    const files = await readdir(sourceFolder);
+    await Promise.all(
+      files.map((file) => {
+        const targetFile = join(targetFolder, file);
+        const sourceFile = join(sourceFolder, file);
+        return copyFile(sourceFile, targetFile);
+      })
+    );
   } catch {
     throw new Error('FS operation failed');
   }
