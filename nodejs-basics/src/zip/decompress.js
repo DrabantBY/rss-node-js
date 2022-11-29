00933b98174ffd3ed005abd69cccb7fef9cdd965
DrabantBY <drabant2022@gmail.com>
2022-11-29 14:24:06 +0300

    feat: implement zip functions

--- nodejs-basics/src/zip/decompress.js
+++ nodejs-basics/src/zip/decompress.js
@@ -1,5 +1,14 @@
+import { getFilePath } from '../getFilePath.js';
+import { createReadStream, createWriteStream } from 'fs';
+import { createUnzip } from 'zlib';
+
 const decompress = async () => {
-    // Write your code here 
+  const archive = getFilePath(import.meta.url, 'files/archive.gz');
+  const targetFile = getFilePath(import.meta.url, 'files/fileToCompress.txt');
+  const readStream = createReadStream(archive);
+  const writeStream = createWriteStream(targetFile);
+  const unzip = createUnzip();
+  readStream.pipe(unzip).pipe(writeStream);
 };
 
 await decompress();
