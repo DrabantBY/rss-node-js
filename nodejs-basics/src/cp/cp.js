88736bda4ca431962312e7e7818db313cc39a46d
DrabantBY <drabant2022@gmail.com>
2022-11-29 15:49:29 +0300

    feat: implement cp function

--- nodejs-basics/src/cp/cp.js
+++ nodejs-basics/src/cp/cp.js
@@ -1,5 +1,13 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
+import { getFilePath } from '../getFilePath.js';
+import { spawn, fork } from 'child_process';
+
+export const spawnChildProcess = async (args) => {
+  const mainFile = getFilePath(import.meta.url, 'files/script.js');
+  // const childProcess = fork(mainFile, args);
+
+  const childProcess = spawn('node', [mainFile, ...args]);
+  process.stdin.pipe(childProcess.stdin);
+  childProcess.stdout.pipe(process.stdout);
 };
 
-spawnChildProcess();
+spawnChildProcess(process.argv);
