9521c90bec232ab103caf60eae6a919dffe5b6f2
DrabantBY <drabant2022@gmail.com>
2022-12-04 21:27:57 +0300

    refactor: update cp function

--- nodejs-basics/src/cp/cp.js
+++ nodejs-basics/src/cp/cp.js
@@ -3,11 +3,11 @@ import { spawn, fork } from 'child_process';
 
 export const spawnChildProcess = async (args) => {
   const mainFile = getFilePath(import.meta.url, 'files/script.js');
-  // const childProcess = fork(mainFile, args);
+  const childProcess = fork(mainFile, args);
 
-  const childProcess = spawn('node', [mainFile, ...args]);
-  process.stdin.pipe(childProcess.stdin);
-  childProcess.stdout.pipe(process.stdout);
+  // const childProcess = spawn('node', [mainFile, ...args]);
+  // process.stdin.pipe(childProcess.stdin);
+  // childProcess.stdout.pipe(process.stdout);
 };
 
 spawnChildProcess(process.argv);
